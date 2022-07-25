c72fa4c611dcff525e4da383b441754445ca812d
mitsue-eth <mitsue.eth@gmail.com>
2022-07-25 22:59:31 +0200

    first commit

---
+++ hardhat/contracts/Exchange.sol
@@ -0,0 +1,22 @@
+//SPDX-License -Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Exchange is ERC20 {
+    address public cryptoDevTokenAddress;
+
+    constructor(address _CryptoDevToken) ERC20("CryptoDev LP Token", "CDLP") {
+        require(
+            _CryptoDevToken != address(0),
+            "Token address passed is a null address"
+        );
+        cryptoDevTokenAddress = _CryptoDevToken;
+    }
+
+    //function returns the amount of ERC20 tokens which are held by this contract
+
+    function getReserve() public view returns (uint256) {
+        return ERC20(cryptoDevTokenAddress).balanceOf(address(this));
+    }
+}
